6277d2b4d7b8dbfd9e51ba6413c73c6b8693afa2
Aner Lucero <4rgento@gmail.com>
2021-06-22 10:26:01 -0300

    Use SimpleFigure in OpenDocument writer.

--- src/Text/Pandoc/Writers/OpenDocument.hs
+++ src/Text/Pandoc/Writers/OpenDocument.hs
@@ -377,7 +377,7 @@ blockToOpenDocument o = \case
     Plain          b -> if null b
                         then return empty
                         else inParagraphTags =<< inlinesToOpenDocument o b
-    Para [Image attr c (s,T.stripPrefix "fig:" -> Just t)] -> figure attr c s t
+    SimpleFigure attr c s t -> figure attr c s t
     Para           b -> if null b &&
                            not (isEnabled Ext_empty_paragraphs o)
                         then return empty
